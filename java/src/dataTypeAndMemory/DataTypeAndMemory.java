db58633d1613482397f9ab4cf0c7ef724a6ef7d6
akhana721 <akhana721@kakonoair>
2023-03-15 18:12:03 +0900

    課題1-16

---
+++ java/src/dataTypeAndMemory/DataTypeAndMemory.java
@@ -0,0 +1,27 @@
+package dataTypeAndMemory;
+
+public class DataTypeAndMemory {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String x = "A";
+		System.out.printf(x);
+		
+		String y = "Hello Java";
+		System.out.printf(y);
+		
+		int fujiElevation = 3776;
+		System.out.printf("富士山の標高は " + fujiElevation + "m です。");
+		
+		double Pi = 3.14;
+		System.out.printf("円周率は" + Pi + "です");
+		
+		int [] nums = {10,100,1000};
+		System.out.println(nums[1]);
+		
+		int[] b = nums;
+	    b[1] = 8;
+	    System.out.println(b[1]);
+	}
+
+}
