31b53517cea27303f3b05c01d1c8b31224050b5e
akhana721 <akhana721@kakonoair>
2023-04-04 17:49:30 +0900

    課題1-29訂正版

---
+++ java/src/utils/PrefectureComparator.java
@@ -0,0 +1,17 @@
+package utils;
+
+import info.Prefecture;
+import java.util.Comparator;
+
+public class PrefectureComparator implements Comparator<Prefecture> {
+    private boolean ascending;
+
+    public PrefectureComparator(boolean ascending) {
+        this.ascending = ascending;
+    }
+
+    @Override
+    public int compare(Prefecture p1, Prefecture p2) {
+        return ascending ? Double.compare(p1.getArea(), p2.getArea()) : Double.compare(p2.getArea(), p1.getArea());
+    }
+}
